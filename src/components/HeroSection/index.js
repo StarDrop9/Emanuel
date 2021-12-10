f8d32fdf3684f42d0d2fc50a98e9283f337f5cb4
Kurt Starck <kurtstarck@yahoo.com>
2021-12-10 06:17:15 -0600

    Updated Parables

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import { Button } from '../ButtonElements';
 // import Video from '../../videos/video.mov';
 import { useMediaQuery } from "react-responsive";
-import Desktophome from "../../components/Desktophome"
-import Mobilehome from "../../components/Mobilehome"
-import { DeviceSize } from "../../components/Responsive"
+import Desktophome from "../../components/Desktophome";
+import Mobilehome from "../../components/Mobilehome";
+import { DeviceSize } from "../../components/Responsive";
 import {
   HeroContainer,
   // HeroBg,
@@ -15,39 +15,45 @@ import {
   HeroBtnWrapper,
   //ArrowForward,
   //ArrowRight
-} from './HeroElements';
+} from "./HeroElements";
 
 function HeroSection() {
   const [hover, setHover] = useState(false);
   const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
 
-  const [parables] = useState(["Be still, and know that I am God",
-  "This is my Son, whom I Love.", "With him I am well pleased!",
-  "You can only come to me through my Son!",
-  "Now this is eternal life: that you know me", "The only true God,", 
-  "And Jesus Christ, whom I have sent",
-  "Listen To the words of my Son","*Love God with all your heart, mind, soul and strength!",
-  "Blessed are the poor in spirit for theirs is the kingdom of heaven.",
-  "Blessed are the pure in heart for they will see God.", 
-  "Blessed are the merciful for they will be shown mercy.",
-  "*Love one and other as I have Loved You!",
-  "I am the way, the truth, and the life. No one comes to the Father, except through me!",
-"Come to me all who are weary and burdened and I will give you rest."])
+  const [parables] = useState([
+    "Be still, and know that I am God",
+    "This is my Son, whom I Love.",
+    "With him I am well pleased!",
+    "You can only come to me through my Son!",
+    "Now this is eternal life: that you know me",
+    "The only true God,",
+    "And Jesus Christ, whom I have sent",
+    "Listen To the words of my Son",
+    "*Love God with all your heart, mind, soul and strength!",
+    "Blessed are the poor in spirit for theirs is the kingdom of heaven.",
+    "Blessed are the pure in heart for they will see God.",
+    "Blessed are the merciful for they will be shown mercy.",
+    "*Love one and other as I have Loved You!",
+    "I am the way, the truth, and the life. No one comes to the Father, except through me!",
+    "Come to me all who are weary and burdened and I will give you rest.",
+    "Consider it pure joy, my brothers and sisters,whenever you face trials of many kinds",
+    "In the darkness of adversity, you are able to see more clearly the radiance of My face.",
+    "Remember that you have an eternity of trouble-free living awaiting you in Heaven.",
+  ]);
 
-
-// eslint-disable-next-line
+  // eslint-disable-next-line
   const onHover = () => {
     setHover(!hover);
   };
   return (
-    <HeroContainer id='home'>
-        
+    <HeroContainer id="home">
       {/* <HeroBg>
         <VideoBg playsInline autoPlay loop muted src={Video} type='video/mov' />
       </HeroBg> */}
       <HeroContent>
-      {!isMobile && <Desktophome parables={parables} />}
-      {isMobile && <Mobilehome parables={parables}/>} 
+        {!isMobile && <Desktophome parables={parables} />}
+        {isMobile && <Mobilehome parables={parables} />}
         {/* <HeroH1>Options Trading made Easy</HeroH1>
         <HeroP>
           Sign up for a new account today and receive $10 in credit towards
