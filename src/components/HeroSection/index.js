d80e024c2fb9c0449f02ba2552f839a2a7a88115
Kurt Starck <kurtstarck@yahoo.com>
2022-02-13 11:21:35 -0600

    Words of Christ Updated

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -45,6 +45,8 @@ function HeroSection() {
     "For in Christ all the fullness of the Deity lives in bodily form, and in Christ you have been brought to fullness. He is the head over every power and authority.",
     "Let everything that has breath praise the Lord.",
     "For you know the grace of our Lord Jesus Christ, that though he was rich, yet for your sake he became poor, so that you through his poverty might become rich.",
+    "Those who hope in the Lord will renew their strength.They will soar on wings like eagles",
+    "Peace I leave with you; my peace I give you. I do not give to you as the world gives. Do not let your hearts be troubled and do not be afraid.",
   ]);
 
   // eslint-disable-next-line
